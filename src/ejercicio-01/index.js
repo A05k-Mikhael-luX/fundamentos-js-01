ae21a117d07307c630646a3c478e02850889f152
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-10-23 20:40:57 +0200

    Añadir commit ejercicio 1 index js

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,12 @@
+const nombre = anastasia
+const edad = 30
+const estudiante = true
+const es_mayor_de_edad = edad >= 18
+
+
+const mensaje= 'hola, mi nombre es + nombre + y tengo + edad+  años. soy estudiante + esEstudiante +'
+// en java se usa camelCase tipo de escritura
+//tb esta el snake_case, el PascalCase y el kebab-case
+// \n es un enter
+console.log(mensaje, mensaje)
+
